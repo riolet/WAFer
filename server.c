d251f5c01a03d3ab8ad68ac9a64bc098de8860ed
rohana <admin@riolet.com>
2014-07-09 18:35:01 -0700

    Prettier server.c

--- server.c
+++ server.c
@@ -3,27 +3,14 @@
 #include <string.h>
 #include "server.h"
 #include "nopeutils.h"
-
-void server_main(int client, const char * url, const char * method) 
+	
+void server_main(int client, const char * request, const char * method) 
 { 
-	
-	 /* Reading and writing headers
-	  * Roll your own if you would like more control */
-	 read_and_discard_headers(client);
-	 print_standard_headers(client);
-	 /* Done reading and writing headers */
-	 
-	 /* Fun begins here */
-	 char buf[1024];
-	 
-	 sprintf(buf, "<HTML><HEAD></HEAD><BODY>Hello World<BR/>");
-	 send(client, buf, strlen(buf), 0);
-
-	 sprintf(buf,"%s", url);
-	 send(client, buf, strlen(buf), 0); 
-	
-	 sprintf(buf, "</BODY></HTML>\r\n");
-	 send(client, buf, strlen(buf), 0);
-	/* All done */
+	  if (strcmp(request,"/")==0) {	 		
+		headers(client); 
+		docwrite(client, "<!DOCTYPE html><HTML><HEAD></HEAD><BODY>");
+		docwrite(client, "<P>Hello World</P>");
+		docwrite(client, "</BODY></HTML>\r\n");
+	 }
 }
 	
