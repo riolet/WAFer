a08252375ef3edb92f42a74fb4f5c7a568bf9c64
rohana <admin@riolet.com>
2014-07-11 10:59:01 -0700

    Better Header Handling

--- server.c
+++ server.c
@@ -1,16 +1,13 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <string.h>
 #include "server.h"
 #include "nopeutils.h"
 	
 void server_main(int client, const char * request, const char * method) 
 { 
 	  if (strcmp(request,"/")==0) {	 		
-		headers(client); 
-		docwrite(client, "<!DOCTYPE html><HTML><HEAD></HEAD><BODY>");
-		docwrite(client, "<P>Hello World</P>");
-		docwrite(client, "</BODY></HTML>\r\n");
+		char **headers=readHeaders(client);
+		nprintf(client, "<!DOCTYPE html><HTML><HEAD></HEAD><BODY>");
+		nprintf(client, "<P>Hello World</P>");
+		nprintf(client, "</BODY></HTML>\r\n");
 	 }
 }
 	
