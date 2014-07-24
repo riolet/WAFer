93386889b89eeda4cf14fd8b0736a387c43f265f
rohana <admin@riolet.com>
2014-07-24 10:28:41 -0700

    Fixed sample server.c

--- server.c
+++ server.c
@@ -1,13 +1,7 @@
 #include "server.h"
 #include "nopeutils.h"
 	
-void server(int client, const char * queryString, const char * method)
-{ 
-	  if (strcmp(queryString,"/")==0) {
-		char **headers=readHeaders(client);
-		nprintf(client, "<!DOCTYPE html><HTML><HEAD></HEAD><BODY>");
-		nprintf(client, "<P>Hello World</P>");
-		nprintf(client, "</BODY></HTML>\r\n");
-	 }
+void server(Request request){ 
+	/*Your code goes here*/
 }
 	
